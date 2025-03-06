8b4f287c1681108580e984f85693fbc89d77cc19
Jfrosty <jfrostybeat@gmail.com>
2025-03-06 00:02:13 -0500

    path update for template

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd src
 python3 src/main.py "/Static/"
--- src/main.py
+++ src/main.py
@@ -6,10 +6,10 @@ from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
 
 
-dir_path_static = ".."
-dir_path_public = "../docs"
-dir_path_content = "../content"
-template_path = "../template.html"
+dir_path_static = "."
+dir_path_public = "./docs"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 
